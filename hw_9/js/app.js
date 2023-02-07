f831a4a7bae34f9fc51b90673ae4abe8949fd43d
o_ebrahim <kangurt.boy@gmail.com>
2023-02-07 17:11:54 +0500

    Выполнил дз Thanos

---
+++ hw_9/js/app.js
@@ -0,0 +1,42 @@
+const posts = [
+  {
+    id: 'tobon',
+    url: 'https://salom.alif.tj/partners/tobon',
+    logoUrl: 'https://alif-skills.pro/media/tobon.svg',
+    title: 'Тобон',
+    category: 'Отопление',
+    period: 12,
+  },
+  {
+    id: 'charme',
+    url: 'https://salom.alif.tj/partners/charme',
+    logoUrl: 'https://alif-skills.pro/media/charme.svg',
+    title: 'Charme',
+    category: 'Женская одежда',
+    period: 3,
+  },
+  {
+    id: 'akika',
+    url: 'https://salom.alif.tj/partners/akika',
+    logoUrl: 'https://alif-skills.pro/media/akika.svg',
+    title: 'Ақиқа',
+    category: 'Ювелирные изделия',
+    period: 6,
+  },
+  {
+    id: 'fosila',
+    url: 'https://salom.alif.tj/partners/fosila',
+    logoUrl: 'https://alif-skills.pro/media/fosila.svg',
+    title: 'Fosila',
+    category: 'Оргтехника',
+    period: 24,
+  },
+];
+
+function filterByThanos(items) {
+  return items.filter((item, index) => index % 2 !== 0);
+}
+
+const filtered = filterByThanos(posts);
+
+console.log(filtered);
